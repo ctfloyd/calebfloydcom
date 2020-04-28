94a57842c04ed060fb66b226c1cc23222e099770
ctfloyd <ctfloyd@wisc.edu>
2020-04-28 01:17:25 -0500

    animations now use gpu rendering

--- calebfloydcom/src/Animation.js
+++ calebfloydcom/src/Animation.js
@@ -21,16 +21,14 @@ export function doAnimate(id, state) {
         targetY = 0.35 * viewPortHeight + window.pageYOffset;
     } else if (viewPortWidth >= 992 && viewPortWidth <= 1199) {
         targetX = 0.37 * viewPortWidth;
-        targetY = 0.35 * viewPortHeight + + window.pageYOffset;
+        targetY = 0.35 * viewPortHeight + window.pageYOffset;
     } else {
         targetX = viewPortWidth / 2;
         targetY = viewPortHeight / 2;
     }
 
-
-
     let deltaX = targetX - myCenterX;
-    let deltaY = Math.abs(targetY - myCenterY);
+    let deltaY = targetY - myCenterY;
     let animationTime = 175; // ms
     let numFrames = Math.ceil(animationTime / animationFrameTime);
     let translateAmountPerFrameX = deltaX / numFrames;
@@ -39,19 +37,26 @@ export function doAnimate(id, state) {
     let scaleAmountPerFrameY = 1.0 / numFrames;
     let currentFrame = 0;
     let modalHandle = document.getElementById(id);
-    let scaleAmountX = 0;
-    let scaleAmountY = 0;
+    let scaleAmountX = scaleAmountPerFrameX;
+    let scaleAmountY = scaleAmountPerFrameY;
+    let translateAmountX = translateAmountPerFrameX;
+    let translateAmountY = translateAmountPerFrameY;
 
     modalHandle.style.left = myCenterX + "px";
     modalHandle.style.top = myCenterY + "px";
-    animateToOpen();
+    console.log(targetX, myCenterX);
+    requestAnimationFrame(animateToOpen);
     function animateToOpen() {
         // TODO: use transform and translate
-        modalHandle.style.left = (Number(modalHandle.style.left.replace("px", '')) + translateAmountPerFrameX) + "px";
-        modalHandle.style.top = (Number(modalHandle.style.top.replace("px", '')) - translateAmountPerFrameY)+ "px";
-        modalHandle.style.transform = `scale(${scaleAmountX + scaleAmountPerFrameX}, ${scaleAmountY + scaleAmountPerFrameY})`;
+        // modalHandle.style.left = (Number(modalHandle.style.left.replace("px", '')) + translateAmountPerFrameX) + "px";
+        // modalHandle.style.top = (Number(modalHandle.style.top.replace("px", '')) - translateAmountPerFrameY)+ "px";
+        //modalHandle.style.transform = `scale(${scaleAmountX + scaleAmountPerFrameX}, ${scaleAmountY + scaleAmountPerFrameY})`;
+        //modalHandle.style.transform = "translate(" + translateAmountX + "," + translateAmountY + ") scale(" + scaleAmountX + "," + scaleAmountY + ")";
+        modalHandle.style.transform = `translate(${translateAmountX}px,${translateAmountY}px) scale(${scaleAmountX},${scaleAmountY})`;
         scaleAmountX += scaleAmountPerFrameX;
         scaleAmountY += scaleAmountPerFrameY;
+        translateAmountX += translateAmountPerFrameX;
+        translateAmountY += translateAmountPerFrameY;
         if(++currentFrame < numFrames) {
             requestAnimationFrame(animateToOpen);
         }
@@ -68,9 +73,16 @@ export function doClose(id, callback) {
     let scaleAmountY = 1;
     let currentFrame = 0;
 
-    animateToClose();
+    let originalTransform = modalHandle.style.transform.split(' ');
+    originalTransform = originalTransform[0] + originalTransform[1];
+    originalTransform = originalTransform.replace("translate(", "").replace(/px/gi, "").replace(/\)/gi, "");
+    let coords = originalTransform.split(",");
+    let transformX = coords[0];
+    let transformY = coords[1];
+
+    requestAnimationFrame(animateToClose);
     function animateToClose() {
-        modalHandle.style.transform = `scale(${scaleAmountX - scaleAmountPerFrameX}, ${scaleAmountY - scaleAmountPerFrameY})`;
+        modalHandle.style.transform = `translate(${transformX}px,${transformY}px) scale(${scaleAmountX - scaleAmountPerFrameX}, ${scaleAmountY - scaleAmountPerFrameY})`;
         scaleAmountX -= scaleAmountPerFrameX;
         scaleAmountY -= scaleAmountPerFrameY;
         if(++currentFrame < numFrames) {
--- calebfloydcom/src/Social.js
+++ calebfloydcom/src/Social.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 
 class Social extends Component {
   render() {
-    let socialClass = this.props.fwdClass || "social zoon";
+    let socialClass = this.props.fwdClass || "social zoom";
     let linkFwd = this.props.linkFwd || "";
     let alt = this.props.alt || "";
     return (
-        <a href={this.props.to} className={linkFwd}><i className={this.props.icon + " " + socialClass}></i>{" " + alt}</a>
+        <a href={this.props.to} className={linkFwd}><i className={this.props.icon + " " + socialClass}></i>{"" + alt}</a>
     );
   }
 }
